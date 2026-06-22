6ff2722d8b014cdcb33ad30bbb079667c9d4644d
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-22 12:25:06 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/AppUser.java
+++ backend/src/main/java/com/example/demo/entity/AppUser.java
@@ -23,8 +23,39 @@ public class AppUser {
  String email;
  @Column(nullable = false)
  String password;
- @
-  
-  
-    
+ @Enumerated(EnumType.STRING)
+ @Column(name="domain_role")
+ domainRole domainRole;
+ 
+ public long getId() {
+    return Id;
+ }
+ public void setId(long id) {
+    Id = id;
+ }
+ public String getUsername() {
+    return username;
+ }
+ public void setUsername(String username) {
+    this.username = username;
+ }
+ public String getEmail() {
+    return email;
+ }
+ public void setEmail(String email) {
+    this.email = email;
+ }
+ public String getPassword() {
+    return password;
+ }
+ public void setPassword(String password) {
+    this.password = password;
+ }
+ public domainRole getDomainRole() {
+    return domainRole;
+ }
+ public void setDomainRole(domainRole domainRole) {
+    this.domainRole = domainRole;
+ }
+
 }
