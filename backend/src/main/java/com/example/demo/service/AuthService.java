08a87aff499a7dbc3df6fbc16cfabbdaf77fa248
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-24 12:19:52 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.AppUserRepository;
 
 @Service
 public class AuthService {
-    
+    @Autowired
+    private AppUserRepository repo;
 }
