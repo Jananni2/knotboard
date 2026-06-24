86d75a12fb3a7cef7f7e485d1ef07729423ac40a
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-24 11:11:15 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/AppUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public class AppUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.AppUser;
+
+public interface AppUserRepository extends JpaRepository<AppUser,Long>{
+
     
-}
+}  
