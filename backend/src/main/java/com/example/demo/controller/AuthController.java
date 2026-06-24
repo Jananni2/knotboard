070cef92b6a0888efd209425cf3eb6b97602249d
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-24 12:27:23 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.AuthService;
@@ -9,5 +10,6 @@ import com.example.demo.service.AuthService;
 public class AuthController {
     @Autowired
 private AuthService service;
+@GetMapping("")
     
 }
