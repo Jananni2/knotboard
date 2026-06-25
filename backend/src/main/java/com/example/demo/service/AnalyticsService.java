b673133089734a01c4826d4600c6cc0b3f44df52
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-25 08:40:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AnalyticsService.java
+++ backend/src/main/java/com/example/demo/service/AnalyticsService.java
@@ -1,10 +1,10 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class AnalyticsService {
-    @Autowired
-    private AnalyticsService ser;
-}
+// @Service
+// public class AnalyticsService {
+//     @Autowired
+//     private AnalyticsService ser;
+// }
