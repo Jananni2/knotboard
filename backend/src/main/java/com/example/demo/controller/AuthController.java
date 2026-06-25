cfb02e75d24a1eeb79a1e3fbd6ffe672f08b29b0
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-25 08:56:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,8 @@ public List<AppUser> getUsers(){
 public AppUser addUser(@RequestBody AppUser user){
     return service.addUser(user);
 }
-@DeleteMapping("/")
+@DeleteMapping("/del/{id}")
+public String delAcc(@PathVariable Long id){
+    return service.delAcc(id);
+}
 }
