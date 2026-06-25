e6fff3575556060da6bb8c94850ebb0d7c4c950f
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-25 09:20:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -16,17 +16,25 @@ import com.example.demo.service.AuthService;
 
 @RestController
 public class AuthController {
-     @Autowired
+
+@Autowired
 private AuthService service;
 
 // @GetMapping("/get")
 // public List<AppUser> getUsers(){
 //     return service.getUsers();
 // }
+
 @PostMapping("/api/auth/register")
 public AppUser addUser(@RequestBody AppUser user){
     return service.addUser(user);
 }
+
+@PostMapping("/api/auth/login")
+public AppUser addUser(@RequestBody AppUser user){
+    return service.addUser(user);
+}
+
 @DeleteMapping("/del/{id}")
 public String delAcc(@PathVariable Long id){
     return service.delAcc(id);
