d10db73cdd7108f967676a82667c9d8c3b2d5cde
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-25 08:38:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardActitvity.java
+++ backend/src/main/java/com/example/demo/entity/BoardActitvity.java
@@ -1,36 +1,36 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.EnumType;
+// import jakarta.persistence.Enumerated;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-@Entity
-@Table(name = "boards")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class BoardActitvity {
-@Id
-@GeneratedValue(strategy =GenerationType.IDENTITY)
-private Long id;
-private String title;
-private String description;
-@Enumerated(EnumType.STRING)
-BoardStatus status;
-@Column(name="max_note_capacity")
-Integer maxNoteCapacity;
-Integer currentNoteCount;
-LocalDateTime createdAt;
-}
+// @Entity
+// @Table(name = "boards")
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Getter
+// @Setter
+// public class BoardActitvity {
+// @Id
+// @GeneratedValue(strategy =GenerationType.IDENTITY)
+// private Long id;
+// private String title;
+// private String description;
+// @Enumerated(EnumType.STRING)
+// BoardStatus status;
+// @Column(name="max_note_capacity")
+// Integer maxNoteCapacity;
+// Integer currentNoteCount;
+// LocalDateTime createdAt;
+// }
