1d15991c6bb9c7301d57f64c170c739cc0843067
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-26 15:34:05 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/AppUser.java
+++ backend/src/main/java/com/example/demo/entity/AppUser.java
@@ -22,16 +22,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "app_users")
 public class AppUser {
+
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
  private long id;
+
  @Column(unique = true,nullable = false)
  private String username;
+
  @Column(unique = true,nullable = false)
  private String email;
+
  @Column(nullable = false)
  @JsonIgnore
  private String password;
+ 
  @Enumerated(EnumType.STRING)
  @Column(name="domain_role")
  private DomainRole domainRole;
@@ -39,6 +44,6 @@ public class AppUser {
  public enum DomainRole{
    FACILITATOR,CONTRIBUTOR,STAKEHOLDER;
  }
- 
+
 
 }
