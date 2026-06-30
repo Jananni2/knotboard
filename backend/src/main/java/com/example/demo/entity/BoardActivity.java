2f568b13d04a48f7ed70a199ad2a27d227213c0d
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:37:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardActivity.java
+++ backend/src/main/java/com/example/demo/entity/BoardActivity.java
@@ -2,10 +2,15 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = )
 public class BoardActivity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,8 +21,10 @@ public class BoardActivity {
 
     @ManyToOne
     private AppUser actor;
-
+    
+    @Column(name="action_description")
     private String actionDescription;
 
+    @Column(name="timestamp")
     private LocalDateTime timestamp;
 }
