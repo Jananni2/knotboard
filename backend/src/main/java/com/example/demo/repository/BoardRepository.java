a3ae001b287695aa2c181705652783630adb5c34
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:52:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BoardRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -5,8 +5,11 @@
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.entity.BrainstormingBoard;
 
+@Repository
 public interface BoardRepository extends JpaRepository<BrainstormingBoard,Long> {
  Page<BrainstormingBoard>findAllByStatus(BrainstormingBoard.BoardStatus status,Pageable pageable);
     
--- backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
+++ backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public class StickyNoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StickyNoteRepository extends JpaRepository<{
     
 }
