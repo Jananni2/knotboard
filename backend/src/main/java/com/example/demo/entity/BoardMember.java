b0b9796be4eef158e9a37a9123ea79388d81ffe6
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 13:42:02 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardMember.java
+++ backend/src/main/java/com/example/demo/entity/BoardMember.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 @Entity
-@
+@Table(nam)
 public class BoardMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
