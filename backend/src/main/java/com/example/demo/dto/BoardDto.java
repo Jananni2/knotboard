8aee39e39ff62ad2b7bc44e35f8a2839cacb8d85
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 15:11:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/BoardDto.java
+++ backend/src/main/java/com/example/demo/dto/BoardDto.java
@@ -1,8 +1,20 @@
 package com.example.demo.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class BoardDto {
     private Long id;
     private String title;
     private String description;
-    private String facilitatorName
+    private String facilitatorName;
+    private String status;
+    private Integer maxNoteCapacity;
+    private Integer currentNoteCount;
+    private LocalDateTime createdAt;
+
 }
