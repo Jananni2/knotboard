819ca8fa0d380e1ed749cc01cc3043107a59f0a3
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 15:14:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/NoteDto.java
+++ backend/src/main/java/com/example/demo/dto/NoteDto.java
@@ -1,8 +1,16 @@
 package com.example.demo.dto;
+import java.time.LocalDateTime;
+
 
 public class NoteDto {
     private Long id;
     private String title;
     private String description;
     private String facilitatorName;
+    private String status;
+    private Integer maxNoteCapacity;
+    private Integer currentNoteCount;
+    private LocalDateTime createdAt;
+
+
 }
