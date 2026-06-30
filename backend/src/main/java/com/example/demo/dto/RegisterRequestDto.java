346fe2a5dbcc864f967f5ae223bd1ef84e64f8ec
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 12:47:39 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/RegisterRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/RegisterRequestDto.java
@@ -1,5 +1,11 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequestDto {
-    
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
