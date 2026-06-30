104b4b474cd7965ec5b1c9ef8f43865fecf19025
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 15:02:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BoardActivityRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardActivityRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
+ 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.BoardActivity;
 
 @Repository
-public interface BoardActivityRepository extends JpaRepository<BoardActivity,Long> {
-    
+public interface BoardActivityRepository extends JpaRepository<BoardActivity,Long>{
+    Page<BoardActivity>findAllByBoardIdOrderByTimestampDesc(Long boardId,Pageable pageable);
 }
