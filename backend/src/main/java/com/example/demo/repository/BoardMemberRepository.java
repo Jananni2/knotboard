b1cee2017a7d5179f5e744a1eac04fafdd4e25f9
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:59:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BoardMemberRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardMemberRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.demo.entity.BoardMember;
 
 @Repository
 public interface BoardMemberRepository extends JpaRepository<BoardMember,Long>{
-  Optional<Board
+  Optional<BoardMember> findByBoardIdAndUserId(Long boardId,Long userId);
 }
