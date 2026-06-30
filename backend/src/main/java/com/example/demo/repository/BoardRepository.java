cb969bc7bb8269b96dc4a87840aa5a34c5adee0e
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:49:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BoardRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -1,13 +1,14 @@
  package com.example.demo.repository;
 
-import org.hibernate.query.Page;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+ 
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.BoardActivity;
 import com.example.demo.entity.BrainstormingBoard;
 
-public interface BoardActivityRepository extends JpaRepository<BoardActivity,Long> {
+public interface BoardRepository extends JpaRepository<BrainstormingBoard,Long> {
  Page<BrainstormingBoard>findAllByStatus(BrainstormingBoard.BoardStatus status,Pageable pageable)
     
  }
