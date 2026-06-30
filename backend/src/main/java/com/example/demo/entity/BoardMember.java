0b7e91eb1e7556b6e725703df12414d894d9881b
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:31:15 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardMember.java
+++ backend/src/main/java/com/example/demo/entity/BoardMember.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-@Table(name = "board_activities")
+@Table(name = "board_members")
 @Data
 @Builder
 @NoArgsConstructor
@@ -26,9 +27,10 @@ public class BoardMember {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_id")
     private BrainstormingBoard board;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private AppUser user;
     
     @Column(name = "access_level")
