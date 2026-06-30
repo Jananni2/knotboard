4775cb200602bfc839bda3800373fdb76aca1481
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:26:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardMember.java
+++ backend/src/main/java/com/example/demo/entity/BoardMember.java
@@ -7,15 +7,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "board_activities")
 @Data
-@
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FE)
     private BrainstormingBoard board;
     @ManyToOne
     private AppUser user;
