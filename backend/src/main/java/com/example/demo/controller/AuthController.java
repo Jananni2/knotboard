a2ae6c3369389d99222d1003e33634154f6c6720
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 15:19:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,44 +1,44 @@
-package com.example.demo.controller;
-// import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+// package com.example.demo.controller;
+// // import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+// // import org.springframework.web.bind.annotation.DeleteMapping;
+// // import org.springframework.web.bind.annotation.GetMapping;
+// // import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// // import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.AppUser;
-import com.example.demo.service.AuthService;
+// import com.example.demo.entity.AppUser;
+// import com.example.demo.service.AuthService;
 
-@RestController
-public class AuthController {
+// @RestController
+// public class AuthController {
 
-@Autowired
-private AuthService service;
+// // @Autowired
+// // private AuthService service;
 
-// @GetMapping("/get")
-// public List<AppUser> getUsers(){
-//     return service.getUsers();
-// }
+// // // @GetMapping("/get")
+// // // public List<AppUser> getUsers(){
+// // //     return service.getUsers();
+// // // }
 
-@PostMapping("/api/auth/register")
-public AppUser addUser(@RequestBody AppUser user){
-    return service.addUser(user);
-}
+// // @PostMapping("/api/auth/register")
+// // public AppUser addUser(@RequestBody AppUser user){
+// //     return service.addUser(user);
+// // }
 
-@PostMapping("/api/auth/login")
-public AppUser loginUser(@RequestBody AppUser user){
-    return service.loginUser(user);
-}
+// // @PostMapping("/api/auth/login")
+// // public AppUser loginUser(@RequestBody AppUser user){
+// //     return service.loginUser(user);
+// // }
 
-// @DeleteMapping("/del/{id}")
-// public String delAcc(@PathVariable Long id){
-//     return service.delAcc(id);
-// }
-// @PutMapping("/update/{id}")
-// public AppUser updatAppUser(@PathVariable Long id,@RequestBody AppUser user){
-//     return service.updatAppUser(id,user);
-// }
-}
+// // // @DeleteMapping("/del/{id}")
+// // // public String delAcc(@PathVariable Long id){
+// // //     return service.delAcc(id);
+// // // }
+// // // @PutMapping("/update/{id}")
+// // // public AppUser updatAppUser(@PathVariable Long id,@RequestBody AppUser user){
+// // //     return service.updatAppUser(id,user);
+// // // }
+// // }
