1f9a0aa5caac1ec92371fa78e58f85319d166826
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 15:10:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/BoardCreateDto.java
+++ backend/src/main/java/com/example/demo/dto/BoardCreateDto.java
@@ -1,6 +1,8 @@
 package com.example.demo.dto;
 
-@
+import lombok.Data;
+
+@Data
 public class BoardCreateDto {
 private String title;
 private String description;
--- backend/src/main/java/com/example/demo/dto/BoardDto.java
+++ backend/src/main/java/com/example/demo/dto/BoardDto.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
 public class BoardDto {
-    
+    private Long id;
+    private String title;
+    private String description;
+    private String facilitatorName
 }
