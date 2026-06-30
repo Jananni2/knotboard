b37f82179ca1aa2e03c13a35b4b055655014c8a0
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 13:11:39 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardActitvity.java
+++ backend/src/main/java/com/example/demo/entity/BoardActitvity.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -31,7 +33,8 @@ private Long id;
 @Column(nullable=false)
 private String title;
 private String description;
-@ManyToOne
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name)
 private AppUser facilitator;
 @Enumerated(EnumType.STRING)
 private BoardStatus status;
