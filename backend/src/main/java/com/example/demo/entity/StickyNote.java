7187bbf23f50a978239d13f6784c79231aa2285b
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 13:17:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/StickyNote.java
+++ backend/src/main/java/com/example/demo/entity/StickyNote.java
@@ -1,5 +1,13 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sticky_notes")
 public class StickyNote {
-    
+    @Id
+    @GeneratedValue(GenerationType.)
 }
