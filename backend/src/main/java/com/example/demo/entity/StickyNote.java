6ad194d68003982f7d14a4db5589f6533ac02ff8
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 13:20:40 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/StickyNote.java
+++ backend/src/main/java/com/example/demo/entity/StickyNote.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,7 +13,12 @@ public class StickyNote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyTo
+    @ManyToOne
     private BrainstormingBoard board;
+    @ManyToOne
+    private AppUser creator;
+    private String content;
+    private String colorCode;
+    private 
 
 }
