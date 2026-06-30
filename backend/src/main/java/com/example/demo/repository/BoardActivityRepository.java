17929336edf43d08340f35490426d3a2d5b71878
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:43:15 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BoardActivityRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardActivityRepository.java
@@ -1,1 +1,10 @@
+ package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.BoardActivity;
+
+public interface BoardActivityRepository extends JpaRepository<BoardActivity,Long> {
  
+    
+ }
