01ca05030e617589666309d5818734261a50d211
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:40:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardActivity.java
+++ backend/src/main/java/com/example/demo/entity/BoardActivity.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,10 +27,12 @@ public class BoardActivity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = )
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="board_id")
     private BrainstormingBoard board;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "actor_id")
     private AppUser actor;
     
     @Column(name="action_description")
