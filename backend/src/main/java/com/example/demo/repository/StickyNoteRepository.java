0952269af1a36bf770a7406e58a956ab55dcf360
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:56:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
+++ backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,5 @@ import com.example.demo.entity.StickyNote;
 @Repository
 public interface StickyNoteRepository extends JpaRepository<StickyNote,Long>{
     @Query("SELECT s FROM StickyNote s WHERE s.board.id = :boardId AND s.deletedAt IS NULL")
-    List
+    List<StickyNote>findAllActivityByBoardId(Long boardId);
 }
