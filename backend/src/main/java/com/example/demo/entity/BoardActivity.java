fd337cdb6861b50ef3ca8a48f9d0ca7d56d51dc4
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:34:15 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BoardActivity.java
+++ backend/src/main/java/com/example/demo/entity/BoardActivity.java
@@ -1,8 +1,16 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 public class BoardActivity {
     @Id
-    @Ge
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private BrainstormingBoard board;
+
+    
 }
