5279b5f7ded1a9b6587ca3f5ae9e5aa2e8b57fb1
JANANNI K <727724eucb019@skcet.ac.in>
2026-06-30 14:53:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
+++ backend/src/main/java/com/example/demo/repository/StickyNoteRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.StickyNote;
+
 @Repository
-public interface StickyNoteRepository extends JpaRepository<{
-    
+public interface StickyNoteRepository extends JpaRepository<StickyNote,Long>{
+    @Q
 }
