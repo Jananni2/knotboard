82ae06941008694876ef0347c77670e65d670f25
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-02 12:13:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,7 +23,8 @@ public class GlobalExceptionHandler {
         public ResponseEntity<Map<String,String>> handleRuntimeException(RuntimeException ex){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message",ex.getMessage()));
         }
-    @ExceptionHandler()
+    @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+    public ResponseEntity<Map<String,String>> 
     
     }
 
