7c686bf4c1f839b2818dc4a35d7fceefcd253b62
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-02 11:40:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.example.demo.exception;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(StickyNoteNotFoundException.class){
-        
+        public ResponseEntity<Map<String,String>> handleNotFound(StickyNoteNotFoundException ex){
+             
+        }
     }
 }
