a7526bee66796fbdd8a0a5b46a9e7a01ce07114d
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-02 12:25:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.demo.exception;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
