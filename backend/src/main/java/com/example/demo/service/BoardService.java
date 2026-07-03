989c253f2452af3895e95b4071b28107335702c5
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-03 14:13:28 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/BoardService.java
+++ backend/src/main/java/com/example/demo/service/BoardService.java
@@ -2,8 +2,15 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.BoardCreateDto;
+import com.example.demo.dto.BoardDto;
+import com.example.demo.entity.AppUser;
+
+import jakarta.transaction.Transactional;
+
 @Service
 public class BoardService{
-      private 
+    @Transactional
+      public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator)
     
 }
