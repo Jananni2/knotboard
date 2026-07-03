8a69617fa15cb5fa249c6784a41e715ed4b25f56
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-03 10:08:50 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,42 +1,42 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// // import java.util.List;
-// // import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.entity.AppUser;
-// import com.example.demo.repository.AppUserRepository;
+import com.example.demo.entity.AppUser;
+import com.example.demo.repository.AppUserRepository;
 
-// @Service
-// public class AuthService {
-//      @Autowired
-//      private AppUserRepository repo;
+@Service
+public class AuthService {
+     @Autowired
+     private AppUserRepository repo;
 
-//     public List<AppUser> getUsers() {
-//         return repo.findAll();
-//     }
+    public List<AppUser> getUsers() {
+        return repo.findAll();
+    }
 
-//     public AppUser addUser(AppUser user) {
-//         return repo.save(user);
-//     }
+    public AppUser addUser(AppUser user) {
+        return repo.save(user);
+    }
 
-//     public AppUser loginUser(AppUser user) {
-//         return repo.save(user);
-//     }
+    public AppUser loginUser(AppUser user) {
+        return repo.save(user);
+    }
 
-//     public String delAcc(Long id) {
-//          repo.deleteById(id);
-//          return "User deleted successfully";
-//     }
+    public String delAcc(Long id) {
+         repo.deleteById(id);
+         return "User deleted successfully";
+    }
 
-//     public AppUser updatAppUser(Long id, AppUser user) {
-//       Optional<AppUser> u = repo.findById(id);
-//       if(u.isPresent()){
+    public AppUser updatAppUser(Long id, AppUser user) {
+      Optional<AppUser> u = repo.findById(id);
+      if(u.isPresent()){
 
-//       }
-//     }
+      }
+    }
 
      
-// }
+}
