80764b3da54041ceaad4f7fb516d293c9efffafd
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-03 14:14:59 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/BoardService.java
+++ backend/src/main/java/com/example/demo/service/BoardService.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Service;
 import com.example.demo.dto.BoardCreateDto;
 import com.example.demo.dto.BoardDto;
 import com.example.demo.entity.AppUser;
+import com.example.demo.entity.BrainstormingBoard;
 
 import jakarta.transaction.Transactional;
 
 @Service
 public class BoardService{
     @Transactional
-      public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator)
+      public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator){
+        BrainstormingBoard board = BrainstormingBoard.builder()
+        .
+      }
     
 }
