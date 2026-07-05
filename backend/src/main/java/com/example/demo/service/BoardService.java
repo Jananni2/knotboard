82ec997c5cdf5850b929d2960b729e6eee7a9975
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-05 21:12:56 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/BoardService.java
+++ backend/src/main/java/com/example/demo/service/BoardService.java
@@ -2,25 +2,25 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-// import com.example.demo.dto.BoardCreateDto;
-// import com.example.demo.dto.BoardDto;
-// import com.example.demo.entity.AppUser;
-// import com.example.demo.entity.BrainstormingBoard;
+import com.example.demo.dto.BoardCreateDto;
+import com.example.demo.dto.BoardDto;
+import com.example.demo.entity.AppUser;
+import com.example.demo.entity.BrainstormingBoard;
 
-// import jakarta.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 @Service
 public class BoardService{
-    // @Transactional
-    //   public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator){
-    //     BrainstormingBoard board = BrainstormingBoard.builder()
-    //     .title(dto.getTitle())
-    //     .description(dto.getDescription())
-    //     .facilitator(facilitator)
-    //     .status( "ACTIVE")
-    //     .maxNoteCapacity(dto.getMaxCapacity())
-    //     .currentNoteCount()
-    //     .createdAt(Lo)
-    //   }
+    @Transactional
+      public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator){
+        BrainstormingBoard board = BrainstormingBoard.builder()
+        .title(dto.getTitle())
+        .description(dto.getDescription())
+        .facilitator(facilitator)
+        .status( "ACTIVE")
+        .maxNoteCapacity(dto.getMaxCapacity())
+        .currentNoteCount()
+        .createdAt(Lo)
+      }
     
 }
