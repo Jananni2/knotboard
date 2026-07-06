fdd27e483033c43156a2f6ee687d7ed5bdce6c3b
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-06 10:13:26 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/BoardService.java
+++ backend/src/main/java/com/example/demo/service/BoardService.java
@@ -25,7 +25,7 @@ public class BoardService{
   private final BoardMemberRepository memberRepository;
   private final BoardActivityRepository boardActivityRepository;
     @Transactional
-      public BoardDto createBoard(BoardCreateDto dto,AppUser facilitator){
+      public BrainstormingBoard createBoard(BoardCreateDto dto,AppUser facilitator){
         BrainstormingBoard board = BrainstormingBoard.builder()
         .title(dto.getTitle())
         .description(dto.getDescription())
