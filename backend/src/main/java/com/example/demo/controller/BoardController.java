2384d3699799e2ff67747b6400921389caa70f7d
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-06 18:05:20 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/BoardController.java
+++ backend/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.BoardDto;
 import com.example.demo.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,5 +17,5 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
-    
+    public ResponseEntity<BoardDto>
 }
