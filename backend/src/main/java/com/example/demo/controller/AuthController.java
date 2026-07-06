d3aab12d386301538f85a5e4ab6f2c9d01df7b38
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-06 10:23:56 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 // import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 // import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.AuthResponseDto;
 import com.example.demo.dto.RegisterRequestDto;
 import com.example.demo.entity.AppUser;
 import com.example.demo.service.AuthService;
@@ -26,7 +28,7 @@ private AuthService service;
 
 @PostMapping("/api/auth/register")
 public AppUser addUser(@RequestBody RegisterRequestDto user){
-    return service.addUser(user);
+    return ResponseEntity<AuthResponseDto> 
 }
 
 @PostMapping("/api/auth/login")
