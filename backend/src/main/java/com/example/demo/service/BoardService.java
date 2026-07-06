743c707880498205a19d628f552a833bf02d0650
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-06 10:01:27 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/BoardService.java
+++ backend/src/main/java/com/example/demo/service/BoardService.java
@@ -56,7 +56,7 @@ public class BoardService{
         
         boardActivityRepository.save(boardActivity);
 
-        // return boardMapp
+        return BoardCreateDto.toDto()
         
     
       }
