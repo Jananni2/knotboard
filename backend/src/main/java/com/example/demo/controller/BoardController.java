ee20801194a23496ed5cde71331017a2adbad59c
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-06 18:14:20 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/BoardController.java
+++ backend/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
-    public ResponseEntity<BoardDto>createBoard(@RequestBody BoardCreateDto dto,AppUser user){
-
+    public ResponseEntity<BoardDto>createBoard(@RequestBody BoardCreateDto dto,@AuthenticationPrincipal AppUser user){
+          return ResponseEntity.status(Cre)
     }
 }
