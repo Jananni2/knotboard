1a2e85b96af47f4d1cdc16c9f77e703af1804d71
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-08 10:54:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/BoardController.java
+++ backend/src/main/java/com/example/demo/controller/BoardController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+// import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
