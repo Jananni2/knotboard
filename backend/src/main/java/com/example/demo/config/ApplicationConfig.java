d008b52e7228e7258c267a983253eb4f26a8d676
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-13 19:09:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/ApplicationConfig.java
+++ backend/src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -39,9 +39,5 @@ public class ApplicationConfig {
         return provider;
     }
 
-    // @Bean
-    // public AuthenticationManager authenticationManager(
-    //         AuthenticationConfiguration config) throws Exception {
-    //     return config.getAuthenticationManager();
-    // }
+     
 }
