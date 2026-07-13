3c9a9f070461ef54022401a862b5493b62f63d06
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-13 19:06:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/ApplicationConfig.java
+++ backend/src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -1,8 +1,6 @@
 package com.example.demo.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
