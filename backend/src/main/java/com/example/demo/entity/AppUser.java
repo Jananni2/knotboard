882d183029435d593b67b374a7b5a4eb23476aa8
JANANNI K <727724eucb019@skcet.ac.in>
2026-07-13 18:48:31 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/AppUser.java
+++ backend/src/main/java/com/example/demo/entity/AppUser.java
@@ -1,6 +1,10 @@
-package com.example.demo.entity;
+ package com.example.demo.entity;
+
+import java.util.Collection;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,11 +13,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
- 
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @NoArgsConstructor
@@ -21,29 +29,64 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @Table(name = "app_users")
-public class AppUser {
+public class AppUser implements UserDetails {
 
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
- @Column(unique = true,nullable = false)
- private String username;
+    @Column(unique = true, nullable = false)
+    private String username;
 
- @Column(unique = true,nullable = false)
- private String email;
+    @Column(unique = true, nullable = false)
+    private String email;
 
- @Column(nullable = false)
- @JsonIgnore
- private String password;
- 
- @Enumerated(EnumType.STRING)
- @Column(name="domain_role")
- private DomainRole domainRole;
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
 
- public enum DomainRole{
-   FACILITATOR,CONTRIBUTOR,STAKEHOLDER;
- }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "domain_role")
+    private DomainRole domainRole;
 
+    public enum DomainRole {
+        FACILITATOR,
+        CONTRIBUTOR,
+        STAKEHOLDER
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + domainRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
