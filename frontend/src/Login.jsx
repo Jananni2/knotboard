80f8ff237526df2ea9e2f863dadf8a6ea917cb5c
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-27 21:41:03 +0530

    User commit

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
- import { login } from "./store/authSlice";
+ import { login } from "./store/slices/authSlice";
 
 function Login() {
     const [username, setUsername] = useState("");
