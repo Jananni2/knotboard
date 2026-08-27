ceb6ad5d18a4924c23d0e9b34b413a6f87dc4056
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-27 22:05:15 +0530

    User commit

---
+++ frontend/src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./slices/authSlice";
+import noteReducer from "./slices/noteSlice";
+import boardReducer from "./slices/boardSlice";
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        notes: noteReducer,
+        boards: boardReducer,
+    },
+});
+
+export default store;
