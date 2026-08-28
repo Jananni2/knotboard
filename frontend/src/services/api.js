fe81d6509a1209d23062476918d3febf2e57ec6c
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-28 21:36:06 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,28 @@
-import axios from "axios";
+ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:8080"
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-    return config;
-});
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response?.status === 401) {
             localStorage.removeItem("token");
+            localStorage.removeItem("user");
             window.location.href = "/login";
         }
 
