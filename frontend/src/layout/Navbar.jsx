44cc26bc4e123d94e6e4d541394692458b6387d8
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-28 14:19:10 +0530

    User commit

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,16 +1,50 @@
  import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../store/slices/authSlice";
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-brand">KnotBoard</div>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-      <div className="navbar-links">
-        <a href="/dashboard">Dashboard</a>
-        <a href="/settings">Settings</a>
-      </div>
-    </nav>
-  );
+    const user = useSelector((state) => state.auth?.user);
+
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate("/login");
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                KnotBoard
+            </div>
+
+            <div className="navbar-links">
+                <a href="/dashboard">Dashboard</a>
+
+                {user?.role === "FACILITATOR" && (
+                    <a href="/settings">Settings</a>
+                )}
+            </div>
+
+            <div className="navbar-user">
+                {user ? (
+                    <>
+                        <span>
+                            Signed in as <strong>{user.username}</strong>
+                        </span>
+
+                        <button onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </>
+                ) : (
+                    <a href="/login">Login</a>
+                )}
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
