c92bfffadebbd8506c9ffc7beeb9c3c6a19f0e05
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-29 09:24:48 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,38 @@
- // api.js
-import axios from "axios";
+ import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8080",
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
 
-// Example: attach token dynamically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("authToken"); // or from Redux/context
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default api;
