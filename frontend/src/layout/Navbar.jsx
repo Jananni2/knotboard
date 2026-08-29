d02cbeeb187cce38c138be9ae3714ddfbdf3839e
JANANNI K <727724eucb019@skcet.ac.in>
2026-08-29 20:37:23 +0530

    User commit

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,19 +1,6 @@
  import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { logout } from "../store/slices/authSlice";
 
 function Navbar() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const user = useSelector((state) => state.auth?.user);
-
-    const handleLogout = () => {
-        dispatch(logout());
-        navigate("/login");
-    };
-
     return (
         <nav className="navbar">
             <div className="navbar-brand">
@@ -21,27 +8,13 @@ function Navbar() {
             </div>
 
             <div className="navbar-links">
-                <a href="/dashboard">Dashboard</a>
+                <a href="/dashboard">
+                    Dashboard
+                </a>
 
-                {user?.role === "FACILITATOR" && (
-                    <a href="/settings">Settings</a>
-                )}
-            </div>
-
-            <div className="navbar-user">
-                {user ? (
-                    <>
-                        <span>
-                            Signed in as <strong>{user.username}</strong>
-                        </span>
-
-                        <button onClick={handleLogout}>
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <a href="/login">Login</a>
-                )}
+                <a href="/settings">
+                    Settings
+                </a>
             </div>
         </nav>
     );
