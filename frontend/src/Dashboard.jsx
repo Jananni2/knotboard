ac6a9c5240a4a4c18e46008820b823d5b0c5e73b
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-03 21:52:27 +0530

    User commit

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,7 +1,7 @@
  import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
-import DomainChart from "./components/common/DomainChart";
+import 
 import {
     fetchBoards,
     createBoard,
@@ -440,7 +440,7 @@ function Dashboard() {
 
                                 </div>
 
-<DomainChart data={domainData} />
+ <DomainChart data={workspaceStats} />
                                 {/* Capacity */}
                                 <div className="form-group">
 
