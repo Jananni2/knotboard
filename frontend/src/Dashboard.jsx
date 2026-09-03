6a0cd290cfa38b3b5884b549b588a2809d1a6fbf
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-03 21:55:27 +0530

    User commit

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,7 +1,7 @@
  import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
-import DomainChart from "/components/common/DomainChart";
+import DomainChart from "./components/dashboard/DomainChart";
 import {
     fetchBoards,
     createBoard,
