1ebce6173f25baa1d69834757c9b3bfba23d7e05
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-03 11:50:04 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
                             element={<Login />}
                         />
 
-                        {/* Dashboard */}
+                        Dashboard
                         <Route
                             path="/"
                             element={<Dashboard />}
