282f6a7ecea41f315c90116c8224f951188dcd20
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-03 11:27:34 +0530

    User commit

--- frontend/src/services/boardService.js
+++ frontend/src/services/boardService.js
@@ -10,7 +10,7 @@ const boardService = {
     // GET /boards?page={page}&size={size}
     getBoards: async (page = 0, size = 6) => {
         const response = await api.get(
-            `/boards?page=${page}&size=${size}`
+            `api/boards?page=${page}&size=${size}`
         );
         return response.data;
     },
