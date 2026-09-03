e4eff5108c9851b679b7df920d1ec6ddeb1444aa
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-03 12:23:04 +0530

    User commit

--- frontend/src/components/dashboard/FacilitatorSettings.jsx
+++ frontend/src/components/dashboard/FacilitatorSettings.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import boardService from "./services/boardService";
+import boardService from "../../services/boardService";
 
 function FacilitatorSettings() {
     const [boards, setBoards] = useState([]);
