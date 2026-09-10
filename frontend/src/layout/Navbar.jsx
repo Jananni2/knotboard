7d5b590969f07b0ea002357fb6cb1e45bd044d9d
JANANNI K <727724eucb019@skcet.ac.in>
2026-09-10 10:09:12 +0530

    User commit

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/slices/authSlice";
 
